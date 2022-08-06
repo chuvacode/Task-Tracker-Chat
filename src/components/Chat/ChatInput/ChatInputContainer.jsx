0128cb9de63b28a96cf19ed2dcf444632944f10b
Alexey <chuvakov.aleshka@yandex.ru>
2022-08-06 19:02:48 +0300

    Update input message

--- src/components/Chat/ChatInput/ChatInputContainer.jsx
+++ src/components/Chat/ChatInput/ChatInputContainer.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import {connect} from "react-redux";
 import ChatInput from "./ChatInput";
 import {sendMessage} from "../../../redux/chat-reducer";
-import {change, reduxForm} from "redux-form";
+import {reduxForm} from "redux-form";
 import {compose} from "redux";
 
-const ChatInputContainer = props => {
+const ChatInputContainer = Component => props => {
   const handleSubmit = formData => {
-    console.log(formData.message);
     props.sendMessage(formData.message)
   };
 
-  return <WithReduxFormChatInput onSubmit={handleSubmit}/>
+  return <Component onSubmit={handleSubmit}/>
 };
-
-let WithReduxFormChatInput = reduxForm({form: 'chat-input'})(ChatInput);
 
 export default compose(
   connect(null, {
-    sendMessage,
-    change
-  })
-)(ChatInputContainer)
+    sendMessage
+  }),
+  ChatInputContainer,
+  reduxForm({form: 'chat-input'})
+)(ChatInput)
 
 
--- src/components/common/FormControls/index.js
+++ src/components/common/FormControls/index.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, {createRef, useEffect} from "react";
 import Style from './FormControlls.module.css';
+import {compose} from "redux";
+import {change} from "redux-form";
+import {connect} from "react-redux";
+import {setEndOfContenteditable} from "../../../utils/setEndOfContenteditable";
 
 export const Input = ({input, meta, ...props}) => {
   const hasError = meta.touched && meta.error;
@@ -14,3 +18,52 @@ export const Input = ({input, meta, ...props}) => {
     </div>
   );
 };
+
+const TextArea = ({input, meta, change, ...props}) => {
+  let refSubmit = createRef();
+  let refTextarea = createRef();
+
+  useEffect(() => {
+    let el = refTextarea.current;
+    refTextarea.current.focus();
+    el.innerText = input.value;
+    setEndOfContenteditable(el);
+
+    if (el.childNodes.length === 1 && el.childNodes[0].tagName === 'BR') {
+      change(meta.form, 'message', '');
+    }
+  });
+
+  const handlerInput = e => {
+    change(meta.form, 'message', e.target.innerText);
+  };
+
+  const handlerKeyPress = e => {
+    if (!e.shiftKey && e.key === 'Enter') {
+      e.preventDefault();
+      refSubmit.current.click();
+    }
+    if (e.target.childNodes.length > 20) {
+      e.preventDefault();
+    }
+  };
+
+  return (<div className={Style.textareaContainer}>
+    <div className={Style.textarea}
+         aria-multiline='true'
+         contentEditable='true'
+         role='textbox'
+         tabIndex='0'
+         onInput={handlerInput}
+         onKeyPress={handlerKeyPress}
+         ref={refTextarea}
+         {...props}>
+    </div>
+    {input.value === '' && <div className={Style.placeholder}>{props.placeholder}</div>}
+    <input {...input} type="text" hidden/>
+    <input {...input} type="submit" ref={refSubmit} hidden/>
+  </div>);
+};
+export const TextAreaContainer = compose(
+  connect(null, {change})
+)(TextArea);
